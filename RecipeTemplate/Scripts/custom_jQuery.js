44ca4a3732bdc943d254533d00bd51f3645bdae7
Stacy Toney <stacy.toney@protonmail.com>
2020-11-29 00:39:22 -0600

    ad space designated, css and jQuery added, grid rearranged

---
+++ RecipeTemplate/Scripts/custom_jQuery.js
@@ -0,0 +1,21 @@
+﻿/*!
+ * jQuery JavaScript Library v3.0.0 -ajax,-ajax/jsonp,-ajax/load,-ajax/parseXML,-ajax/script,-ajax/var/location,-ajax/var/nonce,-ajax/var/rquery,-ajax/xhr,-manipulation/_evalUrl,-event/ajax,-effects,-effects/Tween,-effects/animatedSelector,-deprecated
+ * https://jquery.com/
+ *
+ * Includes Sizzle.js
+ * https://sizzlejs.com/
+ *
+ * Copyright jQuery Foundation and other contributors
+ * Released under the MIT license
+ * https://jquery.org/license
+ *
+ * Date: 2016-06-09T18:03Z
+ */
+
+jQuery(document).ready(function () {
+    console.log("Hello World jQuery");
+});
+
+$("fa-heart-o").click(function () {
+    $(this).toggleClass("fa-heart fa-heart-o");
+});
